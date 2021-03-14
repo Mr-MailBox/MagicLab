46acc3560556e8e1b947252a50c84da297f4a0ea
Mr-MailBox <39784537+Mr-MailBox@users.noreply.github.com>
2021-03-14 18:02:59 -0400

    Add files via upload

--- MagicLab.sh
+++ MagicLab.sh
@@ -6,27 +6,42 @@ echo "░█▄█░█▀█░█▀▀░▀█▀░█▀▀░█░░�
 "
 #Res, RAM, OS, GPU, CPU, Storage
 
+# storage
+# maindrive=`df -h /`
+# used=${maindrive##*on} 
+# used=${maindrive##*       } 
+# echo -e "\e[35mStorage\e[0m --  using $used out of $size"
+
 #resolution
 res=`xdpyinfo | awk '/dimensions/{print $2}'`
 echo -e "\e[35mResolution\e[0m -- ${res##*:}"
 
 # Ram
-MEM=`free -m  | grep Mem | awk '{print $2}'`
+MEM=`free -m  |awk '/^Mem:/ {print $3 " MB/" $2}'`
 echo -e "\e[35mRam\e[0m --  $MEM MB"
 
 #GPU
 GPU=`DRI_PRIME=1 glxinfo -B | grep renderer`
-# echo "GPU -- ${GPU##*:}"
 echo -e "\e[35mGPU\e[0m -- ${GPU##*:}"
 
 #CPU
-CPU=`cat /proc/cpuinfo | sed '5!d'`
+CPU=`grep 'model name' /proc/cpuinfo`
 CPU=${CPU##*:}
 echo -e "\e[35mCPU\e[0m -- $CPU"
 
 #OS
 OS=`hostnamectl`
-OS=${OS%Kernel*}
-OS=${OS##*System}
+OS=${OS%Kernel*} #everything after and including "Kernel" gets deleted
+OS=${OS##*System} #everything before and including "System" gets deleted
 OS=${OS#* }
 echo -e "\e[35mOS\e[0m --  $OS"
+
+
+
+
+
+
+
+
+
+
